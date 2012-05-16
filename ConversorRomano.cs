6265931ecadeb52568061cab2758bb6d9b97bfde
saitodisse <saitodisse@gmail.com>
2012-05-16 16:38:07 -0300

    ateh o 39

---
+++ ConversorRomano.cs
@@ -0,0 +1,141 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NumerosRomanosTeste
+{
+    public class NumeroRomano
+    {
+        public int NumeroDecimal { get; set; }
+        public string NomeRomano { get; set; }
+        public bool Repete { get; set; }
+    }
+
+    public class NumeroRomanoLista : List<NumeroRomano>
+    {
+        public string GetNome (int numero)
+        {
+            NumeroRomano numeroRomano = this.First(x => x.NumeroDecimal == numero);
+            return numeroRomano.NomeRomano;
+        }
+    }
+
+
+
+    public class ConversorRomano
+    {
+        private NumeroRomanoLista _romanos;
+
+        public ConversorRomano()
+        {
+            InicializarRomanos();
+        }
+        private void InicializarRomanos()
+        {
+            _romanos = new NumeroRomanoLista();
+
+            var numeroRomano = new NumeroRomano();
+            numeroRomano.NumeroDecimal = 0;
+            numeroRomano.NomeRomano = "";
+            numeroRomano.Repete = false;
+            _romanos.Add(numeroRomano);
+
+            numeroRomano = new NumeroRomano();
+            numeroRomano.NumeroDecimal = 1;
+            numeroRomano.NomeRomano = "I";
+            numeroRomano.Repete = true;
+            _romanos.Add(numeroRomano);
+
+            numeroRomano = new NumeroRomano();
+            numeroRomano.NumeroDecimal = 5;
+            numeroRomano.NomeRomano = "V";
+            numeroRomano.Repete = false;
+            _romanos.Add(numeroRomano);
+
+            numeroRomano = new NumeroRomano();
+            numeroRomano.NumeroDecimal = 10;
+            numeroRomano.NomeRomano = "X";
+            numeroRomano.Repete = true;
+            _romanos.Add(numeroRomano);
+
+            numeroRomano = new NumeroRomano();
+            numeroRomano.NumeroDecimal = 50;
+            numeroRomano.NomeRomano = "L";
+            numeroRomano.Repete = false;
+            _romanos.Add(numeroRomano);
+
+            numeroRomano = new NumeroRomano();
+            numeroRomano.NumeroDecimal = 100;
+            numeroRomano.NomeRomano = "C";
+            numeroRomano.Repete = true;
+            _romanos.Add(numeroRomano);
+        }
+
+        public string ConverterDecimal(int numero)
+        {
+            if (numero >= 1 && numero <= 5)
+            {
+                return ObterLetras(numero, 1, 0, 5);
+            }
+            if (numero >= 6 && numero <= 10)
+            {
+                return ObterLetras(numero - 5, 1, 5, 10);    
+            }
+            if (numero >= 11 && numero <= 39)
+            {
+                return ObterLetras(numero, 1, 5, 10);
+            }
+            
+            return string.Empty;
+        }
+
+
+
+        private string ObterLetras(int numero, int anterior, int atual, int proximo)
+        {
+            switch (numero)
+            {
+                case 1:
+                    return _romanos.GetNome(atual) + _romanos.GetNome(anterior);
+                case 2:
+                    return _romanos.GetNome(atual) + _romanos.GetNome(anterior) + _romanos.GetNome(anterior);
+                case 3:
+                    return _romanos.GetNome(atual) + _romanos.GetNome(anterior) + _romanos.GetNome(anterior) + _romanos.GetNome(anterior);
+                case 4:
+                    return _romanos.GetNome(anterior) + _romanos.GetNome(proximo);
+                case 5:
+                    return _romanos.GetNome(proximo);
+                default:
+                    int dezena = numero/10;
+                    string primeiroCaracter;
+                    if (dezena < 4)
+                    {
+                        primeiroCaracter = ObterLetras(dezena, 10, 0, 50);
+                    }
+                    else
+                    {
+                        primeiroCaracter = ObterLetras(dezena, 10, 50, 100);
+                    }
+
+                    string segundoCaracter;
+
+                    int unidade = numero % 10;
+                    if (unidade <= 0)
+                    {
+                        return primeiroCaracter;
+                    }
+                    else if (unidade > 5 && unidade <= 10)
+                    {
+                        unidade -= 5;
+                        segundoCaracter = ObterLetras(unidade, 1, 5, 10);
+                        return primeiroCaracter + segundoCaracter;
+                    }
+                    else
+                    {
+                        segundoCaracter = ObterLetras(unidade, 1, 0, 5);
+                        return primeiroCaracter + segundoCaracter;
+                    }
+            }
+        }
+    }
+}
--- NumerosRomanos.cs
+++ NumerosRomanos.cs
@@ -5,101 +5,34 @@ namespace NumerosRomanosTeste
     [TestFixture]
     public class NumerosRomanos
     {
-        [Test]
-        public void Deve_Retornar_I_Para_1()
+        [TestCase(1, "I")]
+        [TestCase(2, "II")]
+        [TestCase(3, "III")]
+        [TestCase(4, "IV")]
+        [TestCase(5, "V")]
+        [TestCase(6, "VI")]
+        [TestCase(7, "VII")]
+        [TestCase(8, "VIII")]
+        [TestCase(9, "IX")]
+        [TestCase(10, "X")]
+        [TestCase(11, "XI")]
+        [TestCase(12, "XII")]
+        [TestCase(13, "XIII")]
+        [TestCase(14, "XIV")]
+        [TestCase(15, "XV")]
+        [TestCase(16, "XVI")]
+        [TestCase(17, "XVII")]
+        [TestCase(18, "XVIII")]
+        [TestCase(19, "XIX")]
+        [TestCase(20, "XX")]
+        [TestCase(21, "XXI")]
+        [TestCase(31, "XXXI")]
+        [TestCase(39, "XXXIX")]
+        [TestCase(40, "XL")]
+        public void Deve_Retornar_I_Para_1(int num, string romano)
         {
-            Assert.AreEqual(Romanos.Converter(1), "I");
-        }
-
-        [Test]
-        public void Deve_Retornar_II_Para_2()
-        {
-            Assert.AreEqual(Romanos.Converter(2), "II");
-        }
-
-        [Test]
-        public void Deve_Retornar_III_Para_3()
-        {
-            Assert.AreEqual(Romanos.Converter(3), "III");
-        }
-
-        [Test]
-        public void Deve_Retornar_IV_Para_4()
-        {
-            Assert.AreEqual(Romanos.Converter(4), "IV");
-        }
-        [Test]
-        public void Deve_Retornar_IV_Para_5()
-        {
-            Assert.AreEqual(Romanos.Converter(5), "V");
-        }
-        [Test]
-        public void Deve_Retornar_IV_Para_6()
-        {
-            Assert.AreEqual(Romanos.Converter(6), "VI");
-        }
-        [Test]
-        public void Deve_Retornar_IV_Para_7()
-        {
-            Assert.AreEqual(Romanos.Converter(7), "VII");
-        }
-        [Test]
-        public void Deve_Retornar_IV_Para_8()
-        {
-            Assert.AreEqual(Romanos.Converter(8), "VIII");
-        }
-        [Test]
-        public void Deve_Retornar_IV_Para_9()
-        {
-            Assert.AreEqual(Romanos.Converter(9), "IX");
-        }
-        [Test]
-        public void Deve_Retornar_IV_Para_10()
-        {
-            Assert.AreEqual(Romanos.Converter(10), "X");
-        }
-
-        [Test]
-        public void Deve_Retornar_XI_Para_11()
-        {
-            Assert.AreEqual(Romanos.Converter(11), "XI");
-        }
-
-        [Test]
-        public void Deve_Retornar_XV_Para_15()
-        {
-            Assert.AreEqual(Romanos.Converter(15), "XV");
-        }
-
-        [Test]
-        public void Deve_Retornar_XIX_Para_19()
-        {
-            Assert.AreEqual(Romanos.Converter(19), "XIX");
-        }
-
-        [Test]
-        public void Deve_Retornar_XX_Para_20()
-        {
-            Assert.AreEqual(Romanos.Converter(20), "XX");
-        }
-
-        [Test]
-        public void Deve_Retornar_XXI_Para_21()
-        {
-            Assert.AreEqual(Romanos.Converter(21), "XXI");
-        }
-
-
-        [Test]
-        public void Deve_Retornar_XXV_Para_25()
-        {
-            Assert.AreEqual(Romanos.Converter(25), "XXV");
-        }
-
-        [Test]
-        public void Deve_Retornar_XXV_Para_33()
-        {
-            Assert.AreEqual(Romanos.Converter(33), "XXXIII");
+            ConversorRomano conv = new ConversorRomano();
+            Assert.AreEqual(romano, conv.ConverterDecimal(num));
         }
     }
 }
